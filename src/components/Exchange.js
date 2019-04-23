0312dbb18e23c04a821475bb57258f85b2fe9a1c
Kosta Korenkov <kosta@whoot.me>
2019-04-23 04:59:36 +0300

    style: xdai,pdai → sunDai

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -1208,7 +1208,7 @@ export default class Exchange extends React.Component {
                  this.setState({xdaiToDendaiMode:"deposit"})
                }}>
                   <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
-                    <i className="fas fa-arrow-up"  /> pDai to {this.props.ERC20NAME}
+                    <i className="fas fa-arrow-up"  /> sunDai to {this.props.ERC20NAME}
                   </Scaler>
                </button>
              </div>
@@ -1218,7 +1218,7 @@ export default class Exchange extends React.Component {
                  this.setState({xdaiToDendaiMode:"withdraw"})
                }}>
                  <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
-                  <i className="fas fa-arrow-down" /> {this.props.ERC20NAME} to pDai
+                  <i className="fas fa-arrow-down" /> {this.props.ERC20NAME} to sunDai
                  </Scaler>
                </button>
              </div>
@@ -2382,7 +2382,7 @@ export default class Exchange extends React.Component {
               <img style={logoStyle} src={this.props.xdai} />
             </div>
             <div className="col-3 p-1" style={{marginTop:8}}>
-              xDai
+              sunDai
             </div>
             <div className="col-4 p-1" style={{marginTop:8,whiteSpace:"nowrap"}}>
                 <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
