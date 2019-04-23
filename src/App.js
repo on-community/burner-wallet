a54b5d000f267781ae2e9a0cef14d520717f1bf7
Johann Barbie <johannbarbie@me.com>
2019-04-23 09:25:14 +0200

    update contract addresses

--- src/App.js
+++ src/App.js
@@ -126,8 +126,8 @@ else if (window.location.hostname.indexOf("burner.leapdao.org") >= 0) {
   ERC20IMAGE = false;
 }
 else if (window.location.hostname.indexOf("sundai.io") >= 0) {
-  XDAI_PROVIDER = "wss://testnet-node1.leapdao.org:1443";
-  WEB3_PROVIDER = "wss://rinkeby.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65";
+  XDAI_PROVIDER = "wss://mainnet-node1.leapdao.org:1443";
+  WEB3_PROVIDER = "wss://mainnet.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65";
   CLAIM_RELAY = false;
   ERC20NAME = false;
   ERC20TOKEN = false;
@@ -405,12 +405,12 @@ class App extends Component {
     setTimeout(this.longPoll.bind(this),150)
 
     // NOTE: Change this to mainnet again when ready for mainnet launch.
-    let mainnetweb3 = new Web3(new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65"));
+    let mainnetweb3 = new Web3(new Web3.providers.WebsocketProvider(WEB3_PROVIDER));
     let ensContract = new mainnetweb3.eth.Contract(require("./contracts/ENS.abi.js"),require("./contracts/ENS.address.js"))
     let daiContract;
     let bridgeContract;
     try{
-      daiContract = new mainnetweb3.eth.Contract(require("./contracts/StableCoin.abi.js"),"0xD2D0F8a6ADfF16C2098101087f9548465EC96C98")
+      daiContract = new mainnetweb3.eth.Contract(require("./contracts/StableCoin.abi.js"),"0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359")
       bridgeContract = new mainnetweb3.eth.Contract(require("./contracts/Bridge.abi.js"), require("./contracts/Bridge.address.js"))
     }catch(e){
       console.log("ERROR LOADING DAI Stablecoin Contract",e)
@@ -1924,7 +1924,7 @@ render() {
           if (state.xdaiweb3) {
             let pdaiContract;
             try{
-              pdaiContract = new state.xdaiweb3.eth.Contract(require("./contracts/StableCoin.abi.js"),"0xeFb369E2c694Bc0ba31945e0D3ac91Ab8E943be3")
+              pdaiContract = new state.xdaiweb3.eth.Contract(require("./contracts/StableCoin.abi.js"),"0x3cC0DF021dD36eb378976142Dc1dE3F5726bFc48")
             }catch(e){
               console.log("ERROR LOADING DAI Stablecoin Contract",e)
             }
@@ -2043,9 +2043,9 @@ async function tokenSend(to, value, gasLimit, txData, cb) {
 
   let receipt;
   if (metaAccount) {
-    receipt = await tokenSendV2(account, to, value, 0, xdaiweb3, web3, metaAccount.privateKey)
+    receipt = await tokenSendV2(account, to, value, 2, xdaiweb3, web3, metaAccount.privateKey)
   } else {
-    receipt = await tokenSendV2(account, to, value, 0, xdaiweb3, web3, null)
+    receipt = await tokenSendV2(account, to, value, 2, xdaiweb3, web3, null)
   }
   // NOTE: The callback cb is not used correctly in the format 
   // cb(error, receipt) throughout the app. We hence cannot send errors in
