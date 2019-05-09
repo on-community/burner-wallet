a9e5032d8837ac97c6485a6c6a1e2235eeffb240
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-05-09 16:12:54 +0200

    Add all other K5 movies

--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -58,7 +58,7 @@ export default class Receive extends React.Component {
     if(receipt.badge){
       sendAmount = (
         <div>
-          <Badge key={"sentbadge"} id={receipt.badge.id} image={receipt.badge.image}/>
+          <Badge key={"sentbadge"} badge={receipt.badge} showName={false}/>
         </div>
       )
     }else{
